338ce5bf40d3fa49d49d71613ec4896b4f3dc71a
Alberto Polanco <prmus10@gmail.com>
2021-03-30 21:30:38 -0600

    Doing nothing. v3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     document.getElementById("tryMe_button").onclick = () => {
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        // window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        window.location.href = "https://www.youtube.com/watch?v=QtBDL8EiNZo";
     };
 }
